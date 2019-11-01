054fccc052a5ffab14cdf89eddc6763149cf63dd
Krantz-XRF <Krantz.XRF@outlook.com>
2019-11-01 21:18:12 +0800

    Improve format
    - more comments for makeOp etc.
    - more readable variable names

--- src/Data/Expression/Op/TH.hs
+++ src/Data/Expression/Op/TH.hs
@@ -48,7 +48,7 @@ makeSynonymSig groupName ops = do
     return $ map (uncurry $ synonymSig expr) ops
            ++ map (uncurry $ synonymSig exprRaw . ('O' : )) ops
 
--- let: <gName> = <groupName:head><ops:name>
+-- let: <gName> = <head groupName><ops:name>
 -- pattern O<ops:name> x ... <ops:cnt>
 --   <- (checkOp -> Just (<gName> x ... <ops:cnt>))
 --   where O<ops:name> x y = liftOp (<gName> x y)
@@ -73,6 +73,11 @@ makeExprSynonym ops = forM ops $ \(nm, cnt) -> do
     return $ PatSynD exprConName (PrefixPatSyn vars) ImplBidir
         (ConP 'Op [ConP opSynName (VarP <$> vars)])
 
+-- let: <gName> = <head groupName><ops:name>
+-- instance EvalOp <groupName> where
+--   eval (<gName> x ... <ops:cnt>)
+--     = <ops:impl> (eval x) ... <ops:cnt>
+--   ... <length ops>
 makeEvalOpInstance :: String -> Name -> Type -> [(String, Int, Name)] -> Q [Dec]
 makeEvalOpInstance groupName varA ctxt ops = do
     let gName = mkName groupName
@@ -90,22 +95,27 @@ fetchConstraintFor var name = reify name >>= \case
     ClassOpI _ typ _ -> process [] [] typ
     _ -> fail "Invalid opImpl: Not supported."
     where
+    -- only allow v or (v :: *)
     viewVarBndr :: TyVarBndr -> Maybe Name
     viewVarBndr (PlainTV v) = Just v
     viewVarBndr (KindedTV v StarT) = Just v
     viewVarBndr _ = Nothing
+    -- only allow forall x . C x => x -> ... -> x
     process :: [Name] -> Cxt -> Type -> Q (Cxt, Int)
     process vars ctxt (ForallT [viewVarBndr -> Just v] newCtxt typ)
         = process (v:vars) (newCtxt ++ ctxt) typ
     process vars ctxt typ = case vars of
         (v:vs) | all (v ==) vs -> processSimple v ctxt 0 typ
         _ -> fail "Only 1 variable should be used."
+    -- the top level forall's are removed
+    -- so here only allow x -> ... -> x
     processSimple :: Name -> Cxt -> Int -> Type -> Q (Cxt, Int)
     processSimple v ctxt cnt (MkArrow (VarT x) typ)
         | x == v = processSimple v ctxt (succ cnt) typ
     processSimple v ctxt cnt (VarT x)
         | x == v = return (replaceVar v ctxt, cnt)
     processSimple _ _ _ _ = fail "Only type a^n -> a is allowed."
+    -- make (C x) becomes (C var)
     replaceVar :: Name -> Cxt -> Cxt
     replaceVar v = map replaceOnce where
         replaceOnce :: Type -> Type
@@ -124,12 +134,12 @@ makeOp groupName ops = do
     let (opNames, opImpls) = unzip ops
     varA <- newName "a"
     (opCxt, opCnt) <- unzip <$> mapM (fetchConstraintFor varA) opImpls
-    let ops2 = zip opNames opCnt
+    let opNamesCnts = zip opNames opCnt
     let ctxtTotal = Set.toList $ Set.fromList $ concat opCxt
     let ctxt = foldl AppT (TupleT $ length ctxtTotal) ctxtTotal
-    groupDecl <- makeOpGroupDecl groupName ops2
-    synonymSig <- makeSynonymSig groupName ops2
-    opSynonym <- makeOpSynonym groupName ops2
-    exprSynonym <- makeExprSynonym ops2
+    groupDecl <- makeOpGroupDecl groupName opNamesCnts
+    synonymSig <- makeSynonymSig groupName opNamesCnts
+    opSynonym <- makeOpSynonym groupName opNamesCnts
+    exprSynonym <- makeExprSynonym opNamesCnts
     evalOpInstance <- makeEvalOpInstance groupName varA ctxt (zip3 opNames opCnt opImpls)
     return $ groupDecl ++ synonymSig ++ opSynonym ++ exprSynonym ++ evalOpInstance
