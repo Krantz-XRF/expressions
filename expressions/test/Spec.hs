adab0afc4c9ab937c29773f832589bff516d9e8d
Krantz-XRF <Krantz.XRF@outlook.com>
2019-11-29 22:16:04 +0800

    Add: Test suite

--- expressions/test/Spec.hs
+++ expressions/test/Spec.hs
@@ -1,2 +1,26 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+module Main where
+
+import Test.Hspec
+
+import Data.Expression
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+type EMonoid = Expression MonoidOp Int
+type EGroup = Expression GroupOp Int
+type ERing = Expression RingOp Int
+type EField = Expression FieldOp Int
+
+spec :: Spec
+spec = describe "Expression Lifting & Checking" $ do
+    it "Lift an Expr with MonoidOp to FieldOp" $
+        liftExpression (OPlus (Atom 1) (Atom 2) :: EMonoid)
+        `shouldBe` (OPlus (Atom 1) (Atom 2) :: EMonoid)
+    it "Unlift an Expr from FieldOp to GroupOp" $
+        checkOp (Minus (Atom 4) (Atom (3 :: Int)) :: FieldOp EField)
+        `shouldBe` Right (Minus (Atom 4) (Atom 3) :: GroupOpDiff EField)
+    it "Cannot unlift to some other Op" $
+        checkOp @MonoidOp (Minus (Atom 4) (Atom (3 :: Int)) :: FieldOp EField)
+        `shouldBe` Left (Minus (Atom 4) (Atom 3))
